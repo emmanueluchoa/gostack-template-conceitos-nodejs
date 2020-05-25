87a4543fe99abbe94fae3b87d660564438e8e866
Emmanuel Cavalcanti <emmanuel.cavalcanti@intelectah.com.br>
2020-05-25 06:23:55 -0300

    Início de implementações

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,11 +11,34 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  try {
+    return response.status(200).json(repositories);
+  } catch (error) {
+    return response.status(400).json({ message: error });
+  }
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  try {
+    let { title, url, techs } = request.body;
+
+    if (!title) throw error("Title must be provided!");
+    if (!url) throw error("Url repository must be provided!");
+    if (!techs) throw error("Techs repository must be provided!");
+
+    let repository = {
+      id: ,
+      title,
+      url,
+      techs,
+      likes: 0
+    };
+
+    repositories.push(repository);
+    return response.status(200).json(repositories);
+  } catch (error) {
+    return response.status(400).json({ message: error });
+  }
 });
 
 app.put("/repositories/:id", (request, response) => {
